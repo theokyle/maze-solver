544e2b1e87391d76ff9217d725dc3271f63f5019
theokyle <theokyle@gmail.com>
2025-05-29 16:29:01 -0500

    corrected maze class

--- maze.py
+++ maze.py
@@ -23,14 +23,16 @@ class Maze ():
         self.__create_cells()
 
     def __create_cells(self):
-        for i in range(self.__num_rows):
+        for i in range(self.__num_cols):
             self.__cells.append([])
-            for j in range(self.__num_cols):
+            for j in range(self.__num_rows):
                 self.__cells[i].append(Cell(self.__win))
+        for i in range(self.__num_cols):
+            for j in range(self.__num_rows):
                 self.__draw_cell(i, j)
     
     def __draw_cell(self, i, j):
-        self.__cells[i][j].draw(i * self.__cell_size_x, j * self.__cell_size_y, (i * self.__cell_size_x) + 1, (j * self.__cell_size_y) + 1)
+        self.__cells[i][j].draw(i * self.__cell_size_x, j * self.__cell_size_y, (i * self.__cell_size_x) + self.__cell_size_x, (j * self.__cell_size_y) + self.__cell_size_y)
         self.__animate()
 
     def __animate(self):
