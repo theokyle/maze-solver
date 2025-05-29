6d9a59d697d0f0d64f684af5a78f667d1ac0d722
theokyle <theokyle@gmail.com>
2025-05-29 15:13:14 -0500

    add point and line classes

--- graphics.py
+++ graphics.py
@@ -4,10 +4,10 @@ class Window ():
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title('Maze Solver')
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -21,3 +21,19 @@ class Window ():
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+class Point ():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line ():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main ():
     win = Window(800, 600)
