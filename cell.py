29ef0ac3669b3f052a03adf50f5b337ce50bb552
theokyle <theokyle@gmail.com>
2025-05-30 11:00:57 -0500

    add break walls functions

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from graphics import Window, Line, Point
 
 class Cell ():
-    def __init__(self, win):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -11,8 +11,11 @@ class Cell ():
         self.__y1 = -1
         self.__y2 = -1
         self.__win = win
+        self.visited = False
 
     def draw(self, x1, y1, x2, y2):
+        if self.__win is None:
+            return
         self.__x1 = x1
         self.__y1 = y1
         self.__x2 = x2
@@ -24,15 +27,27 @@ class Cell ():
         if self.has_left_wall:
             line = Line(p1, p2)
             self.__win.draw_line(line)
+        else:
+            line = Line(p1, p2)
+            self.__win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(p1, p4)
             self.__win.draw_line(line)
+        else:
+            line = Line(p1, p4)
+            self.__win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(p4, p3)
             self.__win.draw_line(line)
+        else:
+            line = Line(p4, p3)
+            self.__win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(p2, p3)
             self.__win.draw_line(line)
+        else:
+            line = Line(p2, p3)
+            self.__win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         if not undo:
--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 from cell import Cell
 import time
+import random
 
 class Maze ():
     def __init__ (
@@ -10,7 +11,8 @@ class Maze ():
         num_cols,
         cell_size_x,
         cell_size_y,
-        win
+        win = None,
+        seed = None
     ):
         self.__x1 = x1
         self.__y1 = y1
@@ -21,6 +23,10 @@ class Maze ():
         self.__win = win
         self.__cells = []
         self.__create_cells()
+        self.__break_entrance_and_exit()
+        if seed:
+            random.seed(seed)
+        self.__break_walls_r(0,0)
 
     def __create_cells(self):
         for i in range(self.__num_cols):
@@ -32,10 +38,66 @@ class Maze ():
                 self.__draw_cell(i, j)
     
     def __draw_cell(self, i, j):
-        self.__cells[i][j].draw(i * self.__cell_size_x, j * self.__cell_size_y, (i * self.__cell_size_x) + self.__cell_size_x, (j * self.__cell_size_y) + self.__cell_size_y)
+        if self.__win is None:
+            return
+        x1 = self.__x1 + i * self.__cell_size_x
+        y1 = self.__y1 + j * self.__cell_size_y
+        x2 = x1 + self.__cell_size_x
+        y2 = y1 + self.__cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
         self.__animate()
 
     def __animate(self):
+        if self.__win is None:
+            return
         self.__win.redraw()
         time.sleep(.05)
+    
+    def __break_entrance_and_exit(self):
+        if len(self.__cells) == 0:
+            return
+        self.__cells[0][0].has_top_wall = False
+        self.__draw_cell(0,0)
+        self.__cells[self.__num_cols-1][self.__num_rows-1].has_bottom_wall = False
+        self.__draw_cell(self.__num_cols-1, self.__num_rows-1)
+
+    def __break_walls_r(self, i, j):
+        self.__cells[i][j].visited = True
+        while True:
+            queue = []
+            if i > 0:
+                if self.__cells[i-1][j].visited == False:
+                    queue.append([i-1, j])
+            if j > 0:
+                if self.__cells[i][j-1].visited == False:
+                    queue.append([i, j-1])
+            if i + 1 < self.__num_cols:
+                if self.__cells[i+1][j].visited == False:
+                        queue.append([i + 1, j])
+            if j + 1 < self.__num_rows:
+                if self.__cells[i][j+1].visited == False:
+                        queue.append([i, j+1])
+            if len(queue) == 0:
+                self.__draw_cell(i, j)
+                return
+            direction = random.randrange(len(queue))
+            next_index = queue[direction]
+
+            if next_index[0] == i + 1:
+                self.__cells[i][j].has_right_wall = False
+                self.__cells[i+1][j].has_left_wall = False
+            if next_index[0] == i - 1:
+                self.__cells[i][j].has_left_wall = False
+                self.__cells[i-1][j].has_right_wall = False
+            if next_index[1] == j+1:
+                self.__cells[i][j].has_bottom_wall = False
+                self.__cells[i][j+1].has_top_wall = False
+            elif next_index[1] == j-1:
+                self.__cells[i][j].has_top_wall = False
+                self.__cells[i][j-1].has_bottom_wall = False
+
+            self.__break_walls_r(next_index[0], next_index[1])
+                
+        
+                
 
